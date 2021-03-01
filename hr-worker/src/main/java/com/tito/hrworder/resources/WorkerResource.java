f5f9feddf5bc4744fd95ed9216027239616f3838
francitonioferreiraalves <francitonio@gmail.com>
2021-03-01 12:37:35 -0300

    db config server

--- hr-worker/src/main/java/com/tito/hrworder/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/tito/hrworder/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -23,23 +22,18 @@ import com.tito.hrworder.repository.WorkerRepository;
 public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
-
-	@Value("${test.config}")
-	private String testConfig;
-
 
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		logger.info("CONFIG = " + testConfig);
+//		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
-	}		
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -53,12 +47,9 @@ public class WorkerResource {
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
 		/*
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		*/
+		 * try { Thread.sleep(3000L); } catch (InterruptedException e) {
+		 * e.printStackTrace(); }
+		 */
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
